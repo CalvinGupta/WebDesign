ecb2a9b0f0587c67c3b8b5b9214f8e59fc68ddbc
CalvinGupta <calverton4@gmail.com>
2025-11-13 15:05:36 -0500

    Added Challenge Project: Number Guesser

---
+++ Building Interactive Websites/JavaScript Syntax, Part I/Challenge Project Number Guesser/script.js
@@ -0,0 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget() {
+  return Math.floor(Math.random()*10);
+}
+
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+  if (Math.abs(humanGuess - targetNumber) <= Math.abs(computerGuess - targetNumber)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function updateScore(winner) {
+  if (winner == "human") {
+    humanScore++;
+  }
+  else {
+    computerScore++;
+  }
+}
+
+function advanceRound() {
+  currentRoundNumber++;
+}
